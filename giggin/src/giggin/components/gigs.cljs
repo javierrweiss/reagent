665592f9a4bdee2d7052b8661c8cbf94cd524dc8
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-08-26 14:16:09 -0300

    completar curso

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,16 +1,27 @@
 (ns giggin.components.gigs
-  (:require [giggin.state :as state]))
+  (:require [giggin.state :as state]
+            [giggin.helpers :refer [format-price]]))
  
 (defn gigs 
   []
   [:main 
-   [:div.gigs (map (fn [gig] 
-                     [:div.gig {:key (:id gig)}
-                      [:img.gig__artwork {:src (:img gig) :alt (:title gig)}]
-                      [:div.gig__body 
-                       [:div.gig__title 
-                        [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
-                         [:i.icon.icon--plus]] (:title gig)]
-                       [:p.gig__price (:price gig)]
-                       [:p.gig__desc (:desc gig)]]]) 
-                   (vals @state/gigs))]])
+   [:div.gigs (doall (map (fn [{:keys [id title price desc img]}] 
+                            [:div.gig {:key id}
+                             [:img.gig__artwork {:src img :alt title}]
+                             [:div.gig__body 
+                              [:div.gig__title 
+                               [:div.btn.btn--primary.float--right.tooltip 
+                                {:data-tooltip "Add to order"
+                                 :on-click #(swap! state/orders update id inc)}
+                                [:i.icon.icon--plus]] title]
+                              [:p.gig__price (format-price price)] 
+                              [:p.gig__desc desc]]]) 
+                          (vals @state/gigs)))]])
+
+(comment
+  
+ (seq @state/orders)
+
+  )
+
+
--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -1,5 +1,44 @@
-(ns giggin.components.orders)
+(ns giggin.components.orders 
+  (:require [giggin.state :as state]
+            [giggin.helpers :refer [format-price]]))
+
+(defn total
+  []
+  (->> @state/orders
+       (map (fn [[id cant]]
+              (* (get-in @state/gigs [id :price]) cant)))
+       (apply +)))
 
 (defn orders
   []
-  [:orders "Ordenes"])
+  [:aside
+   (if (empty? @state/orders)
+     [:div.empty
+      [:div.title "You do not have any orders"]
+      [:div.subtitle "Click on a + to add an order"]]
+     [:div.order
+      [:div.body
+       (for [[id cant] @state/orders]
+         (let [[image titulo precio] ((juxt :img :title :price) (-> @state/gigs id))]
+           [:div.item {:key id}
+            [:div.img
+             [:img {:src image
+                    :alt titulo}]]
+            [:div.content
+             [:p.title (str titulo " " \u00D7 cant)]]
+            [:div.action
+             [:div.price (format-price (* precio cant))]
+             [:button.btn.btn--link.tooltip {:data-tooltip "Remove"
+                                             :on-click #(swap! state/orders dissoc id)}
+              [:i.icon.icon--cross]]]]))]
+      [:div.total
+       [:hr]
+       [:div.item
+        [:div.content "Total"]
+        [:div.action
+         [:div.price (format-price (total))]]
+        [:button.btn.btn--link.tooltip {:data-tooltip "Remove all"
+                                        :on-click #(reset! state/orders {})}
+         [:i.icon.icon--delete]]]]])])
+
+ 
